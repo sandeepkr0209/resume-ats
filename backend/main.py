1ef9928fab951dd6ca0040e58c95489949e4fdd0
sandeepkr0209 <sankrdeep7510@gmail.com>
2026-09-03 00:52:34 +0530

    fix main.py and set the corsmiddleware_origin to correct path

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.ALLOWED_ORIGINS,
+    allow_origins=[
+        "http://localhost:5173",
+        "https://resume-ats-alpha-lyart.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
