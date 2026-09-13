37a58295acbe3e15bef5f3b6990de18eb5f9c888
sandeepkr0209 <sankrdeep7510@gmail.com>
2026-09-13 20:33:52 +0530

    fix the backend main.py and replace corsmiddleware with latest link of frontend

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://resume-ats-alpha-lyart.vercel.app",
+        "https://resume-e2sngx28a-sandeepkr0209s-projects.vercel.app/analyze",
     ],
     allow_credentials=True,
     allow_methods=["*"],
