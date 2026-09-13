b620af08b80dbcdf805d2c0ac4c7a940bec347c4
sandeepkr0209 <sankrdeep7510@gmail.com>
2026-09-13 20:41:23 +0530

    Fix CORS

--- backend/main.py
+++ backend/main.py
@@ -21,6 +21,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "https://resume-e2sngx28a-sandeepkr0209s-projects.vercel.app/analyze",
+        "https://resume-e2sngx28a-sandeepkr0209s-projects.vercel.app/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
