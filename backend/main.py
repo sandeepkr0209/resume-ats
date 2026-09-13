a268fee3ef1f4531ae3ed785c92666b6c95f0434
sandeepkr0209 <sankrdeep7510@gmail.com>
2026-09-13 20:47:47 +0530

    FIX CORS

--- backend/main.py
+++ backend/main.py
@@ -23,6 +23,7 @@ app.add_middleware(
         "https://resume-e2sngx28a-sandeepkr0209s-projects.vercel.app/analyze",
         "https://resume-e2sngx28a-sandeepkr0209s-projects.vercel.app/",
         "https://resume-ats-alpha-lyart.vercel.app/analyze",
+        "https://resume-ats-alpha-lyart.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
